d19177c7647b8dfde60588e38503918b0145cd72
BenBlair0 <157416689+BenBlair0@users.noreply.github.com>
2024-01-22 22:55:59 -0800

    NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -4,13 +4,13 @@
       v-if="player.playing"
       class="now-playing"
       :class="getNowPlayingClass()"
-      v-show="!fade"
     >
       <div class="now-playing__cover">
         <img
           :src="player.trackAlbum.image"
           :alt="player.trackTitle"
           class="now-playing__image"
+          :class="{ fade: fade }" <!-- Conditionally apply fade class -->
         />
       </div>
       <div class="now-playing__details">
@@ -18,8 +18,7 @@
         <h2 class="now-playing__artists" v-text="getTrackArtists"></h2>
       </div>
     </div>
-    <div v-else class="now-playing" :class="getNowPlayingClass()" @transitionend="handleTransitionEnd">
-      <!-- ... (rest of your existing content) -->
+    <div v-else class="now-playing" :class="getNowPlayingClass()">
     </div>
   </div>
 </template>
@@ -44,7 +43,7 @@ export default {
       playerData: this.getEmptyPlayer(),
       colourPalette: '',
       swatches: [],
-      fade: true,
+      fade: false // Added fade variable
     }
   },
 
@@ -64,7 +63,7 @@ export default {
 
   methods: {
     async getNowPlaying() {
-      let data = {}
+      let data = {};
 
       try {
         const response = await fetch(
@@ -74,46 +73,50 @@ export default {
               Authorization: `Bearer ${this.auth.accessToken}`
             }
           }
-        )
+        );
 
         if (!response.ok) {
-          throw new Error(`An error has occurred: ${response.status}`)
+          throw new Error(`An error has occurred: ${response.status}`);
         }
 
         if (response.status === 204) {
-          data = this.getEmptyPlayer()
-          this.playerData = data
+          data = this.getEmptyPlayer();
+          this.playerData = data;
 
           this.$nextTick(() => {
-            this.$emit('spotifyTrackUpdated', data)
-          })
+            this.$emit('spotifyTrackUpdated', data);
+          });
 
-          return
+          return;
         }
 
-        data = await response.json()
-        this.playerResponse = data
+        data = await response.json();
+        this.playerResponse = data;
       } catch (error) {
-        this.handleExpiredToken()
+        this.handleExpiredToken();
 
-        data = this.getEmptyPlayer()
-        this.playerData = data
+        data = this.getEmptyPlayer();
+        this.playerData = data;
 
         this.$nextTick(() => {
-          this.$emit('spotifyTrackUpdated', data)
-        })
+          this.$emit('spotifyTrackUpdated', data);
+        });
       }
     },
 
     getNowPlayingClass() {
-      const playerClass = this.player.playing ? 'active' : 'idle'
-      const backgroundClass = this.player.playing ? '' : 'black-background'
-      return `now-playing--${playerClass} ${backgroundClass}`
+      const playerClass = this.player.playing ? 'active' : 'idle';
+      const backgroundClass = this.player.playing ? '' : 'black-background';
+
+      // Set fade to true when playing to trigger fade-in effect
+      this.fade = this.player.playing;
+
+      return `now-playing--${playerClass} ${backgroundClass}`;
     },
 
     getAlbumColours() {
       if (!this.player.trackAlbum?.image) {
-        return
+        return;
       }
 
       Vibrant.from(this.player.trackAlbum.image)
@@ -121,8 +124,8 @@ export default {
         .clearFilters()
         .getPalette()
         .then(palette => {
-          this.handleAlbumPalette(palette)
-        })
+          this.handleAlbumPalette(palette);
+        });
     },
 
     getEmptyPlayer() {
@@ -132,26 +135,26 @@ export default {
         trackArtists: [],
         trackId: '',
         trackTitle: ''
-      }
+      };
     },
 
     setDataInterval() {
-      clearInterval(this.pollPlaying)
+      clearInterval(this.pollPlaying);
       this.pollPlaying = setInterval(() => {
-        this.getNowPlaying()
-      }, 2500)
+        this.getNowPlaying();
+      }, 2500);
     },
 
     setAppColours() {
       document.documentElement.style.setProperty(
         '--color-text-primary',
         this.colourPalette.text
-      )
+      );
 
       document.documentElement.style.setProperty(
         '--colour-background-now-playing',
         this.colourPalette.background
-      )
+      );
     },
 
     handleNowPlaying() {
@@ -159,19 +162,17 @@ export default {
         this.playerResponse.error?.status === 401 ||
         this.playerResponse.error?.status === 400
       ) {
-        this.handleExpiredToken()
-
-        return
+        this.handleExpiredToken();
+        return;
       }
 
       if (this.playerResponse.is_playing === false) {
-        this.playerData = this.getEmptyPlayer()
-
-        return
+        this.playerData = this.getEmptyPlayer();
+        return;
       }
 
       if (this.playerResponse.item?.id === this.playerData.trackId) {
-        return
+        return;
       }
 
       this.playerData = {
@@ -185,64 +186,60 @@ export default {
           title: this.playerResponse.item.album.name,
           image: this.playerResponse.item.album.images[0].url
         }
-      }
+      };
     },
 
     handleAlbumPalette(palette) {
       let albumColours = Object.keys(palette)
         .filter(item => {
-          return item === null ? null : item
+          return item === null ? null : item;
         })
         .map(colour => {
           return {
             text: palette[colour].getTitleTextColor(),
             background: palette[colour].getHex()
-          }
-        })
+          };
+        });
 
-      this.swatches = albumColours
+      this.swatches = albumColours;
 
       this.colourPalette =
-        albumColours[Math.floor(Math.random() * albumColours.length)]
+        albumColours[Math.floor(Math.random() * albumColours.length)];
 
       this.$nextTick(() => {
-        this.setAppColours()
-      })
+        this.setAppColours();
+      });
     },
 
     handleExpiredToken() {
-      clearInterval(this.pollPlaying)
-      this.$emit('requestRefreshToken')
-    },
-
-    handleTransitionEnd() {
-      this.fade = true;
-    },
+      clearInterval(this.pollPlaying);
+      this.$emit('requestRefreshToken');
+    }
   },
 
   watch: {
     auth: function(oldVal, newVal) {
       if (newVal.status === false) {
-        clearInterval(this.pollPlaying)
+        clearInterval(this.pollPlaying);
       }
     },
 
     playerResponse: function() {
-      this.handleNowPlaying()
+      this.handleNowPlaying();
     },
 
     playerData: function() {
-      this.$emit('spotifyTrackUpdated', this.playerData)
+      this.$emit('spotifyTrackUpdated', this.playerData);
 
       this.$nextTick(() => {
-        this.getAlbumColours()
-      })
+        this.getAlbumColours();
+      });
 
       // Set fade to false to trigger the fade effect
       this.fade = false;
     }
   }
-}
+};
 </script>
 
 <style src="@/styles/components/now-playing.scss" lang="scss" scoped></style>
