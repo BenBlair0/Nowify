5a793560b4be164f3a6e19e1e57892de12ab5fd6
BenBlair0 <157416689+BenBlair0@users.noreply.github.com>
2024-01-25 18:58:05 -0800

    NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -17,10 +17,8 @@
         <h2 class="now-playing__artists" v-text="getTrackArtists"></h2>
       </div>
     </div>
-    <div v-else class="now-playing no-music-playing" :class="{ 'no-music-playing': !isMusicPlaying }">
-      <h1 class="now-playing__idle-heading" v-if="!isMusicPlaying">
-        No music is playing.
-      </h1>
+    <div v-else class="now-playing" :class="getNowPlayingClass()">
+      <h1 class="now-playing__idle-heading">No music is playing 😔</h1>
     </div>
   </div>
 </template>
@@ -42,15 +40,17 @@ export default {
       playerResponse: {},
       playerData: this.getEmptyPlayer(),
       colourPalette: '',
-      swatches: ''
+      swatches: [],
+      bodyBackgroundColorClass: ''
     }
   },
   computed: {
     getTrackArtists() {
       return this.player.trackArtists.join(', ')
     },
-    isMusicPlaying() {
-      return this.player.playing;
+    getNowPlayingClass() {
+      const playerClass = this.player.playing ? 'active' : 'idle'
+      return `now-playing--${playerClass}`
     }
   },
   mounted() {
@@ -61,7 +61,8 @@ export default {
   },
   methods: {
     async getNowPlaying() {
-      let data = {}
+      let data = {};
+
       try {
         const response = await fetch(
           `${this.endpoints.base}/${this.endpoints.nowPlaying}`,
@@ -70,44 +71,48 @@ export default {
               Authorization: `Bearer ${this.auth.accessToken}`
             }
           }
-        )
+        );
+
         if (!response.ok) {
-          throw new Error(`An error has occurred: ${response.status}`)
+          throw new Error(`An error has occurred: ${response.status}`);
         }
+
         if (response.status === 204) {
-          data = this.getEmptyPlayer()
-          this.playerData = data
+          data = this.getEmptyPlayer();
+          this.playerData = data;
+
           this.$nextTick(() => {
-            this.$emit('spotifyTrackUpdated', data)
-          })
-          return
+            this.$emit('spotifyTrackUpdated', data);
+          });
+
+          return;
         }
-        data = await response.json()
-        this.playerResponse = data
+
+        data = await response.json();
+        this.playerResponse = data;
       } catch (error) {
-        this.handleExpiredToken()
-        data = this.getEmptyPlayer()
-        this.playerData = data
+        this.handleExpiredToken();
+
+        data = this.getEmptyPlayer();
+        this.playerData = data;
+
         this.$nextTick(() => {
-          this.$emit('spotifyTrackUpdated', data)
-        })
+          this.$emit('spotifyTrackUpdated', data);
+        });
       }
-    },
-    getNowPlayingClass() {
-      const playerClass = this.player.playing ? 'active' : 'idle'
-      return `now-playing--${playerClass}`
     },
     getAlbumColours() {
       if (!this.player.trackAlbum?.image) {
-        return
+        return;
       }
+
       Vibrant.from(this.player.trackAlbum.image)
         .quality(1)
         .clearFilters()
         .getPalette()
         .then(palette => {
-          this.handleAlbumPalette(palette)
-        })
+          this.handleAlbumPalette(palette);
+        });
     },
     getEmptyPlayer() {
       return {
@@ -116,39 +121,45 @@ export default {
         trackArtists: [],
         trackId: '',
         trackTitle: ''
-      }
+      };
     },
     setDataInterval() {
-      clearInterval(this.pollPlaying)
+      clearInterval(this.pollPlaying);
       this.pollPlaying = setInterval(() => {
-        this.getNowPlaying()
-      }, 2500)
+        this.getNowPlaying();
+      }, 2500);
     },
     setAppColours() {
       document.documentElement.style.setProperty(
         '--color-text-primary',
         this.colourPalette.text
-      )
+      );
+
       document.documentElement.style.setProperty(
         '--colour-background-now-playing',
         this.colourPalette.background
-      )
+      );
+
+      document.body.className = this.bodyBackgroundColorClass;
     },
     handleNowPlaying() {
       if (
         this.playerResponse.error?.status === 401 ||
         this.playerResponse.error?.status === 400
       ) {
-        this.handleExpiredToken()
-        return
+        this.handleExpiredToken();
+        return;
       }
+
       if (this.playerResponse.is_playing === false) {
-        this.playerData = this.getEmptyPlayer()
-        return
+        this.playerData = this.getEmptyPlayer();
+        return;
       }
+
       if (this.playerResponse.item?.id === this.playerData.trackId) {
-        return
+        return;
       }
+
       this.playerData = {
         playing: this.playerResponse.is_playing,
         trackArtists: this.playerResponse.item.artists.map(
@@ -160,48 +171,63 @@ export default {
           title: this.playerResponse.item.album.name,
           image: this.playerResponse.item.album.images[0].url
         }
-      }
+      };
     },
     handleAlbumPalette(palette) {
       let albumColours = Object.keys(palette)
         .filter(item => {
-          return item === null ? null : item
+          return item === null ? null : item;
         })
         .map(colour => {
           return {
             text: palette[colour].getTitleTextColor(),
             background: palette[colour].getHex()
-          }
-        })
-      this.swatches = albumColours
+          };
+        });
+
+      this.swatches = albumColours;
+
       this.colourPalette =
-        albumColours[Math.floor(Math.random() * albumColours.length)]
+        albumColours[Math.floor(Math.random() * albumColours.length)];
+
       this.$nextTick(() => {
-        this.setAppColours()
-      })
+        this.setAppColours();
+      });
     },
     handleExpiredToken() {
-      clearInterval(this.pollPlaying)
-      this.$emit('requestRefreshToken')
+      clearInterval(this.pollPlaying);
+      this.$emit('requestRefreshToken');
     }
   },
   watch: {
     auth: function(oldVal, newVal) {
       if (newVal.status === false) {
-        clearInterval(this.pollPlaying)
+        clearInterval(this.pollPlaying);
       }
     },
     playerResponse: function() {
-      this.handleNowPlaying()
+      this.handleNowPlaying();
     },
     playerData: function() {
-      this.$emit('spotifyTrackUpdated', this.playerData)
+      this.$emit('spotifyTrackUpdated', this.playerData);
+
       this.$nextTick(() => {
-        this.getAlbumColours()
-      })
+        this.getAlbumColours();
+      });
+    },
+    bodyBackgroundColorClass: function() {
+      this.setAppColours();
     }
   }
 }
 </script>
 
-<style src="@/styles/components/now-playing.scss" lang="scss" scoped></style>
+<style src="@/styles/components/now-playing.scss" lang="scss" scoped>
+body.playing {
+  background-color: var(--colour-background-now-playing);
+}
+
+body.paused {
+  background-color: black;
+}
+</style>
